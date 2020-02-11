63a52047a2a6b05660c5010d767c9456ca7ea6ae
Tristan <tristan.doehl@holy-mail.de>
2020-02-11 16:10:15 +0100

    Chore: updating version patch

--- dist/hmc-hooks.esm.js
+++ dist/hmc-hooks.esm.js
@@ -233,7 +233,6 @@ var uuidv4_5 = uuidv4_1.empty;
 var uuidv4_6 = uuidv4_1.fromString;
 
 var rid = function () { return uuidv4_2(); };
-//# sourceMappingURL=rid.js.map
 
 /**
  * A function that always returns `false`. Any passed in parameters are ignored.
@@ -10102,7 +10101,6 @@ var contains$2 = function () {
     }
     return contains$1.apply(R, args);
 };
-//# sourceMappingURL=ram.js.map
 
 var parseUrlSegments = function (url, segments) {
     if (isNil(segments)) {
@@ -10111,13 +10109,11 @@ var parseUrlSegments = function (url, segments) {
     var segs = keys(segments).map(function (seg) { return replace("{" + seg + "}", segments[seg]); });
     return compose.apply(void 0, segs)(url);
 };
-//# sourceMappingURL=parseUrlSegments.js.map
 
 var config = {
     httpKey: 'httpv3',
     formKey: 'formv3',
 };
-//# sourceMappingURL=config.js.map
 
 function defaultEqualityCheck(a, b) {
   return a === b;
@@ -10266,7 +10262,6 @@ var selectPaginationMapper = createSelector(selectNotAction, secondArg, function
 var selectSortMapper = createSelector(selectNotAction, secondArg, function (state, id) { return pathOr$1({}, ['sortMapper'], state); });
 var selectFilter = createSelector(selectNotAction, secondArg, function (state, id) { return pathOr$1({}, ['filter'], state); });
 var selectIsPaginated = createSelector(selectNotAction, secondArg, function (state, id) { return pathOr$1(false, ['paginated'], state); });
-//# sourceMappingURL=useDataSelectors.js.map
 
 var defaultPaginationMapper = {
     fromData: {
@@ -10288,7 +10283,6 @@ var defaultPaginationMapper = {
         page: 'page'
     }
 };
-//# sourceMappingURL=defaultPaginationMapper.js.map
 
 var defaultSortMapper = {
     strategy: 'two-field',
@@ -10297,7 +10291,6 @@ var defaultSortMapper = {
     asc: 'ASC',
     desc: 'DESC'
 };
-//# sourceMappingURL=defaultSortMapper.js.map
 
 var sortMapToParams = function (sortMapper, values) {
     var _a;
@@ -10313,9 +10306,8 @@ var sortMapToParams = function (sortMapper, values) {
         console.error("Unrecognized sort-strategy: " + strategy + " in @hmc/hooks. Check your registerRequest function and your sortMapper value");
     }
 };
-//# sourceMappingURL=sortMapToParams.js.map
 
-var n=function(n){return void 0===n},e=function(n){return Array.isArray(n)},t=function(n){return n&&"number"==typeof n.size&&"string"==typeof n.type&&"function"==typeof n.slice},s=function(o,i,r,f){return (i=i||{}).indices=!n(i.indices)&&i.indices,i.nullsAsUndefineds=!n(i.nullsAsUndefineds)&&i.nullsAsUndefineds,i.booleansAsIntegers=!n(i.booleansAsIntegers)&&i.booleansAsIntegers,r=r||new FormData,n(o)?r:(null===o?i.nullsAsUndefineds||r.append(f,""):"boolean"==typeof o?r.append(f,i.booleansAsIntegers?o?1:0:o):e(o)?o.length?o.forEach(function(n,e){s(n,i,r,f+"["+(i.indices?e:"")+"]");}):r.append(f+"[]",""):o instanceof Date?r.append(f,o.toISOString()):o!==Object(o)||function(n){return t(n)&&"string"==typeof n.name&&("object"==typeof n.lastModifiedDate||"number"==typeof n.lastModified)}(o)||t(o)?r.append(f,o):Object.keys(o).forEach(function(n){var t=o[n];if(e(t))for(;n.length>2&&n.lastIndexOf("[]")===n.length-2;)n=n.substring(0,n.length-2);s(t,i,r,f?f+"["+n+"]":n);}),r)};//# sourceMappingURL=index.mjs.map
+var n=function(n){return void 0===n},e=function(n){return Array.isArray(n)},t=function(n){return n&&"number"==typeof n.size&&"string"==typeof n.type&&"function"==typeof n.slice},s=function(o,i,r,f){return (i=i||{}).indices=!n(i.indices)&&i.indices,i.nullsAsUndefineds=!n(i.nullsAsUndefineds)&&i.nullsAsUndefineds,i.booleansAsIntegers=!n(i.booleansAsIntegers)&&i.booleansAsIntegers,r=r||new FormData,n(o)?r:(null===o?i.nullsAsUndefineds||r.append(f,""):"boolean"==typeof o?r.append(f,i.booleansAsIntegers?o?1:0:o):e(o)?o.length?o.forEach(function(n,e){s(n,i,r,f+"["+(i.indices?e:"")+"]");}):r.append(f+"[]",""):o instanceof Date?r.append(f,o.toISOString()):o!==Object(o)||function(n){return t(n)&&"string"==typeof n.name&&("object"==typeof n.lastModifiedDate||"number"==typeof n.lastModified)}(o)||t(o)?r.append(f,o):Object.keys(o).forEach(function(n){var t=o[n];if(e(t))for(;n.length>2&&n.lastIndexOf("[]")===n.length-2;)n=n.substring(0,n.length-2);s(t,i,r,f?f+"["+n+"]":n);}),r)};
 
 var REGISTER_REQUEST = 'http/useRequest/registerRequest';
 var CHANGE_REQUEST = 'http/useRequest/changeRequest';
@@ -10520,7 +10512,6 @@ var requestReducer = function (state, action) {
         }
     }
 };
-//# sourceMappingURL=requestDuck.js.map
 
 var useRequest = function (_a) {
     var id = _a.id, template = _a.template;
@@ -10667,14 +10658,12 @@ var useRequest = function (_a) {
             onPageSelect: onPageSelect,
             onPrev: onPrev }) }, requestData);
 };
-//# sourceMappingURL=useRequest.js.map
 
 var useData = function (_a) {
     var id = _a.id;
     var data = useSelector(function (state) { return selectData(state, id); });
     return data;
 };
-//# sourceMappingURL=useData.js.map
 
 var createDeepEqualSelector$1 = createSelectorCreator(defaultMemoize, equals$1);
 var selectFormState = function (state) { return prop$1(config.formKey, state); };
@@ -10696,7 +10685,6 @@ var selectFormValid = createSelector(selectFields, secondArg$1, function (fields
     }, true);
 });
 var selectField = createSelector(selectFormState, secondArg$1, thirdArg, function (state, formId, name) { return path$1([formId, 'fields', name], state); });
-//# sourceMappingURL=formSelectors.js.map
 
 var REGISTER_FORM = 'form/useForm/registerForm';
 var UNSET_FORM = 'form/useForm/unsetForm';
@@ -10954,7 +10942,6 @@ var validators = {
         return !isNil$1(v) && !isEmpty$1(v);
     }
 };
-//# sourceMappingURL=validators.js.map
 
 var useRequest$1 = useRequest;
 var useData$1 = useData;
